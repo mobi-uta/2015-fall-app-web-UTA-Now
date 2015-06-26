fcaa81aa6d80a07c693f34bac54ef2c168fd149b
Cameron Moreau <cameron.cm6@gmail.com>
2015-06-26 18:23:36 -0500

    Added map but its buggy

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,27 @@
 var app = angular.module('starter.controllers', ['ngOpenFB']);
 app.controller('AccountCtrl', function ($scope, $ionicModal, $timeout, ngFB) {
 	$scope.fbLogin = function () {
-    ngFB.login({scope: 'email,read_stream,publish_actions'}).then(
-        function (response) {
-            if (response.status === 'connected') {
-                console.log('Facebook login succeeded');
-                $scope.closeLogin();
-            } else {
-                alert('Facebook login failed');
-            }
-        });
-    };
+		ngFB.login({scope: 'email,read_stream,publish_actions'}).then(
+			function (response) {
+				if (response.status === 'connected') {
+					console.log('Facebook login succeeded');
+					$scope.closeLogin();
+				} else {
+					alert('Facebook login failed');
+				}
+			});
+	};
 });
-app.controller('DashCtrl', function($scope) {});
 
 app.controller('EventListController', function($scope, $ionicTabsDelegate, $ionicSlideBoxDelegate) {
 
-  $scope.tabSlideChange = function() {
-    $ionicTabsDelegate.select($ionicSlideBoxDelegate.currentIndex());
-  };
+	$scope.tabSlideChange = function() {
+		$ionicTabsDelegate.select($ionicSlideBoxDelegate.currentIndex());
+	};
 
-  $scope.tabSelect = function(index) {
-    $ionicSlideBoxDelegate.slide(index);
-  };
+	$scope.tabSelect = function(index) {
+		$ionicSlideBoxDelegate.slide(index);
+	};
 });
 
 app.controller('EventsCtrl', function($scope, Events) {
@@ -52,8 +51,17 @@ app.controller('EventsCtrl', function($scope, Events) {
  //        };
 
 });
+
 app.controller('EventDetailCtrl', function($scope, $stateParams) {
 	$scope.eventName = "Event Name";
 	$scope.eventInfo = "This is an event information. It will contain all the cool stuffs about the event.";
-  $scope.goingPeopleIdList = [4,5,6,7,10,11,13,34,234,456,5676,2334,322,100,4989];
+	$scope.goingPeopleIdList = [4,5,6,7,10,11,13,34,234,456,5676,2334,322,100,4989];
+
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'uta-mobi.mibf1hja',
+      accessToken: 'pk.eyJ1IjoidXRhLW1vYmkiLCJhIjoiNTU0N2FiOWM2NjEyMzUyNjc4NTg5M2I1MGM0YjM2N2IifQ.S4guINAIENtuxT6KVlId-g'
+  }).addTo(map);
 });
