6feaff014ee4109e353cd821198e572c1e057f69
Cameron Moreau <cameron.cm6@gmail.com>
2015-07-10 17:20:33 -0500

    Fixed fb id token

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.se
 
 
 app.run(function($ionicPlatform, ngFB) {
-  ngFB.init({appId: 408131292703702 });
+  ngFB.init({appId: 1625138201105249 });
   $ionicPlatform.ready(function() {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
