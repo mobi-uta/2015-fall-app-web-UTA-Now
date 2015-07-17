2fc9a7b84e923cae13492aeb35350e2925486d13
Cameron Moreau <cameron.cm6@gmail.com>
2015-07-17 14:56:16 -0500

    Added slide menu

--- www/js/app.js
+++ www/js/app.js
@@ -5,64 +5,74 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.se
 app.value('fbAccessToken', '');
 
 app.run(function($ionicPlatform, ngFB, fbAccessToken, $location) {
-  ngFB.init({appId: 1625138201105249, accessToken: fbAccessToken });
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleLightContent();
-          }
-    /* Check if user has ever opened the app before
-     * If not, open intro screen
-     **/
-    if(!window.localStorage['lastVisit']) {
-      $location.path('intro');
-    }      
-  });
+	ngFB.init({appId: 1625138201105249, accessToken: fbAccessToken });
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleLightContent();
+		}
+	/* Check if user has ever opened the app before
+	 * If not, open intro screen
+	 **/
+	 if(!window.localStorage['lastVisit']) {
+	 	$location.path('intro');
+	 }      
+	});
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
+	$stateProvider
 
-  .state('intro', {
-    url: '/intro',
-    templateUrl: 'templates/intro.html',
-    controller: 'IntroController'
-  })
+	.state('main', {
+		url: '/main',
+		templateUrl: 'templates/sidemenu.html',
+		abstract: true
+	})
 
-  .state('events-list', {
-    url: '/events',
-    templateUrl: 'templates/events-list.html',
-    controller: 'EventListController'
-  })
+	.state('intro', {
+		url: '/intro',
+		templateUrl: 'templates/intro.html',
+		controller: 'IntroController'
+	})
 
-  .state('event', {
-    url: '/event/:eventId',
-    templateUrl: 'templates/event-detail.html',
-    controller: 'EventDetailCtrl'
-  })
+	.state('main.events-list', {
+		url: '/events',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/events-list.html',
+				controller: 'EventListController'
+			}
+		}
+	})
 
-  .state('settings', {
-    url: '/settings',
-    templateUrl: 'templates/settings.html',
-    controller: 'AccountCtrl'
-  })
+	.state('event', {
+		url: '/event/:eventId',
+		templateUrl: 'templates/event-detail.html',
+		controller: 'EventDetailCtrl'
+	})
 
-  .state('find-event', {
-    url: '/find-event',
-    templateUrl: 'templates/find-event.html',
-    controller: 'FindEventCtrl'
-  })
+	.state('settings', {
+		url: '/settings',
+		templateUrl: 'templates/settings.html',
+		controller: 'AccountCtrl'
+	})
 
-  .state('add-event', {
-    url: '/add-event',
-    templateUrl: 'templates/add-event.html',
-    controller: 'AddEventCtrl'
-  });
+	.state('find-event', {
+		url: '/find-event',
+		templateUrl: 'templates/find-event.html',
+		controller: 'FindEventCtrl'
+	})
 
-  $urlRouterProvider.otherwise('/events');
+	.state('add-event', {
+		url: '/add-event',
+		templateUrl: 'templates/add-event.html',
+		controller: 'AddEventCtrl'
+	});
+
+	$urlRouterProvider.otherwise('/main/events');
 
 });
